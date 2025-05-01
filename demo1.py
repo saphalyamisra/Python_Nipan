c1b1ee9cbe8835d3d1f3f93a05cc8da9d8053968
saphalyamisra <misrasaphalya@gmail.com>
2025-05-01 18:37:03 +0530

    Uncommented

--- demo1.py
+++ demo1.py
@@ -1,13 +1,13 @@
-# age = int(input("Enter your age to check your muscularity! -->"))
-#
-# if age > 32:
-#     name = input("Wow! you are a holy man, please enter your name -->")
-#     print(f"{name} the great Alexander! get married soon! Other wise you can't find a girl")
-# elif age == 32:
-#     exicited_name = input("Excited to know your name, please enter your name -->")
-#     print(f"{exicited_name} our chocopie! What you are waiting for? Do you think you are a kid! Go search a girl soon to get married this year only")
-# else:
-#     print("Chill! There is time, we will find one for you!")
+age = int(input("Enter your age to check your muscularity! -->"))
+
+if age > 32:
+    name = input("Wow! you are a holy man, please enter your name -->")
+    print(f"{name} the great Alexander! get married soon! Other wise you can't find a girl")
+elif age == 32:
+    exicited_name = input("Excited to know your name, please enter your name -->")
+    print(f"{exicited_name} our chocopie! What you are waiting for? Do you think you are a kid! Go search a girl soon to get married this year only")
+else:
+    print("Chill! There is time, we will find one for you!")
 
 
 def new_function_test(s):
